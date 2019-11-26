400516f0184764a16b44e7b4824cb4cf240cff49
zuliani71 <dzuliani@inogs.it>
2019-11-26 16:09:10 +0000

    modular naming

--- jacobi_initial.c
+++ jacobi_initial.c
@@ -1,57 +1,108 @@
+/* Jacobi solver for Laplace equation.
+ * Description: This code solves the Laplace equation using Jacobi iterative
+ *              method on a square grid, given the boundary conditions
+ *
+ * Author: G.P. Brandino
+ * Copyright: 2019 eXact-lab s.r.l.
+ * License: GPLv2
+ */
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-  size_t i,j,it;
-  double *m,*m1,*mTmp;
-  size_t dim=0,iters=0;
-  size_t dimByte=0;
+void setBoundaryConditions(double* grid, double* gridNew, int dimension){
+  // Note that the borders are never modified by the update, so we need to
+  // set up b.c. on BOTH the buffers
+  int i;
+  double increment = 100.0 / ( dimension + 1 );
 
-  if(argc!=3) {
-	fprintf(stderr,"\nwrong number of arguments. Usage: ./a.out dim iterations \n");
-	return 1;
+  for( i=1; i <= dimension + 1; ++i ) {
+      grid[ i * ( dimension + 2 ) ] = i * increment;
+      grid[ ( ( dimension + 1 ) * ( dimension + 2 ) ) + ( dimension + 1 - i ) ] = i * increment;
+      gridNew[ i * ( dimension + 2 ) ] = i * increment;
+      gridNew[ ( ( dimension + 1 ) * ( dimension + 2 ) ) + ( dimension + 1 - i ) ] = i * increment;
   }
-  
-  dim=atoi(argv[1]);
-  iters=atoi(argv[2]);
-  printf("m size = %zu\n", dim);
-  printf("number of iter = %zu\n", iters);
-  dimByte=sizeof(double)*(dim+2)*(dim+2);
-  m=(double*)malloc(dimByte);
-  m1=(double*)malloc(dimByte);
-  
-  for(i=1;i<(dim+2)*(dim+2);++i)
-  
-  m[i]=0;
-  double incr=100.0/(dim+1);
-  for(i=1;i<=dim+1;++i){
-	m[i*(dim+2)]=i*incr;
-	m[((dim+1)*(dim+2))+(dim+1-i)]=i*incr;
-	m1[i*(dim+2)]=i*incr;
-	m1[((dim+1)*(dim+2))+(dim+1-i)]=i*incr;
+
+}
+
+
+void update(double* grid, double* gridNew, int dimension){
+
+  int i,j;
+  for( i = 1 ; i <= dimension; ++i ) {
+      for( j = 1; j <= dimension; ++j ) {
+          gridNew[ ( i * ( dimension + 2 ) ) + j ] = 0.25 *
+          ( grid[ ( ( i - 1 ) * ( dimension + 2 ) ) + j ]
+          + grid[ ( i * ( dimension + 2 ) ) + ( j + 1 ) ]
+          + grid[ ( ( i + 1 ) * ( dimension + 2 ) ) + j ]
+          + grid[ ( i * ( dimension + 2 ) ) + ( j - 1 ) ] );
+      }
   }
-  
-  for(it=0;it<iters;++it){
-	for(i=1;i<=dim;++i)
-	for(j=1;j<=dim;++j)
-	m1[(i*(dim+2))+j]=(0.25)*(m[((i-1)*(dim+2))+j]+m[(i*(dim+2))+(j+1)]+m[((i+1)*(dim+2))+j]+m[(i*(dim+2))+(j-1)]);
-	mTmp=m;
-	m=m1;
-	m1=mTmp;
+}
+
+void printOutput(double* grid, int dimension) {
+
+  int i,j;
+  FILE *file;
+  file = fopen( "solution.dat", "w+" );
+
+  for( i = 0; i < dimension + 2; ++i ){
+      for( j = 0; j < dimension + 2; ++j ){
+          fprintf(file, "%f ", grid[ ( i * ( dimension + 2 ) ) + j ] );
+      }
+      fprintf(file, "\n");
   }
-  
-  FILE *f;
-  f=fopen( "solution.dat", "w+" );
-  
-  for(i=0;i<dim+2;++i){
-	for(j=0;j<dim+2;++j){
-		fprintf(f,"%f ",m[(i*(dim+2))+j]);
-	}
-	fprintf(f,"\n");
-  }
-  fclose(f);
-  free(m);
-  free(m1);
-  return 0;
+
+  fclose( file );
+}
+
+int main(int argc, char* argv[]){
+
+    size_t i, j, it;
+    double *grid, *gridNew, *gridTmp;
+
+    size_t dimension = 0, iterations = 0;
+    size_t dimensionByte = 0;
+
+    // check on input parameters
+    if(argc != 3) {
+        fprintf(stderr,"\nwrong number of arguments. Usage: ./a.out dimension iterations \n");
+        return 1;
+    }
+
+    dimension = atoi(argv[1]);
+    iterations = atoi(argv[2]);
+
+    printf("m size = %zu\n", dimension);
+    printf("number of iter = %zu\n", iterations);
+
+    dimensionByte = sizeof(double) * ( dimension + 2 ) * ( dimension + 2 );
+    grid = ( double* )malloc( dimensionByte );
+    gridNew = ( double* )malloc( dimensionByte );
+
+    //fill initial values
+    for( i = 0; i < (dimension + 2) * (dimension + 2); ++i ) {
+        grid[i] = 0;
+        gridNew[i] = 0;
+    }
+
+    // set up boundary conditions
+    setBoundaryConditions(grid, gridNew, dimension);
+
+    // start the iterations of the algorithm
+    for( it = 0; it < iterations; ++it ) {
+
+        update(grid, gridNew, dimension);
+        // swap the pointers
+        gridTmp = grid;
+        grid = gridNew;
+        gridNew = gridTmp;
+    }
+
+    printOutput(grid, dimension);
+
+    free( grid );
+    free( gridNew );
+
+    return 0;
 }
